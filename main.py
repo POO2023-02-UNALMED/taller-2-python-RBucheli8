560c97c6ea0324d08e8034c09f07d29f990915d3
RBucheli8 <141966641+RBucheli8@users.noreply.github.com>
2023-08-20 21:05:56 -0500

    main

---
+++ main.py
@@ -0,0 +1,43 @@
+class Auto:
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        return len(self.asientos)
+
+    def verificarIntegridad(self):
+        return True
+
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        coloresValidos = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if color in coloresValidos:
+            self.color = color
+
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        tiposValidos = ["electrico", "gasolina"]
+        if tipo in tiposValidos:
+            self.tipo = tipo
